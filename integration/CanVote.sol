e8fc9655e93271751a67aed98d280ffe3dbe73db
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-05-12 15:30:53 +0200

    fixed import error

--- integration/CanVote.sol
+++ integration/CanVote.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 
-import { CanVoteWithoutStarting } from "./CanVoteUtils.sol";
+import { CanVoteWithoutStarting } from "./CanVotePrimitive.sol";
 
 abstract contract CanVote is CanVoteWithoutStarting {
 
