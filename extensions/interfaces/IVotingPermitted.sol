8f13c0e7ef08f4883ba58cab65121aa4dd900de7
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-06-28 15:01:21 +0200

    updated the VotingPermitted interface

--- extensions/interfaces/IVotingPermitted.sol
+++ extensions/interfaces/IVotingPermitted.sol
@@ -9,9 +9,3 @@ interface IVotingPermitted {
     function votingPermitted(uint256 identifier) external view returns(bool permitted);
 }
 
-interface IImplementingPermitted {
-    /// @dev Checks whether the current voting instance permits implementation of the result. This is customizable.
-    /// @param identifier the index for the voting instance in question
-    /// @return permitted a boolean flag that is raised when the voting instance permits implementation of the result.
-    function implementingPermitted(uint256 identifier) external view returns(bool permitted);
-}
