833d88f6f28e5442edb3eef4c5b12479ae4c0031
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-06-30 16:02:05 +0200

    add a Yes/No/Abstain Vote

---
+++ extensions/primitives/CastYesNoAbstainVote.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.4;
+
+
+
+contract CastYesNoAbstainVote {
+
+    enum VoteOptions {no, yes, abstain}
+
+    mapping(uint256=>uint256[3]) internal _vote;
+
+    function _castVote(uint256 identifier, VoteOptions option, uint256 amount) internal {
+        // Since solidity 0.8.0 this will throw an error for amounts bigger than 2^(255-1), as it should!
+        _vote[identifier][uint256(option)] += amount;
+    }
+
+    function _getVotes(uint256 identifier) internal view returns(uint256[3] memory _votes) {
+        return _vote[identifier];
+    }
+}
