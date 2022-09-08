2c5bcc5b82087ec7d5133cd91d51c22b3d7288e4
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-09-08 03:42:53 +0100

    wip on deployment scripts

---
+++ scripts/deployment/playground.ts
@@ -0,0 +1,116 @@
+import { ethers, network } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { durations, types } from "../utils/playgroundVotingContracts";
+import { deploymentInfoPath, basePath } from "../utils/paths";
+import { ContractDeploymentInfo } from "../interfaces/deployment"
+
+import fs from 'fs'
+
+import {
+  VotingPlayground
+} from "../../typechain"
+
+
+async function deployPlaygroundAndBadge(
+  signer: SignerWithAddress,
+  minQuorum: number, 
+  registryAddress: string, 
+  nftWeightedVotingAddress: string, 
+  otherVotingAddress: string, 
+  verbosity: number): Promise<ContractDeploymentInfo> {
+    
+    if (verbosity>0) console.log("--> Start Deployment")
+    let contractName : string;
+
+    const abi = ethers.utils.defaultAbiCoder;
+    
+    let info : ContractDeploymentInfo = {}
+    
+
+    // prelude to voting playground deployment
+    let BadgeFactory = await ethers.getContractFactory("PlaygroundVotingBadge")
+    const playgroundMockup = await ethers.getContractAt("VotingPlayground", ethers.constants.AddressZero);
+    let playgroundInterface = playgroundMockup.interface;
+    
+    let flagAndSelectors : Array<string> = []
+    let votingContracts : Array<string> = []
+    let minDurations : Array<number> = []
+    let minQuorums : Array<number> = []
+    let badgeWeightedVote : Array<boolean> = []
+
+    let functions = Object.keys(types)
+    for (let i=0; i<functions.length; i++){
+        let fct : string = functions[i].toString();
+        flagAndSelectors.push(
+            (types[fct].security=="secure" ? "0x01": "0x00") + playgroundInterface.getSighash(fct).slice(2,)
+        )
+        votingContracts.push(
+            types[fct].badgeWeightedVote ? nftWeightedVotingAddress : otherVotingAddress
+        )
+        badgeWeightedVote.push(types[fct].badgeWeightedVote as boolean)
+        if (types[fct].duration)
+        minDurations.push(types[fct].duration as number)
+        minQuorums.push(minQuorum)
+    }
+
+    let deployArgumentArray = ["Playground Voting Badge", "PLAY"]
+    let deployArguments = abi.encode(["string", "string"], deployArgumentArray)
+    let rawByteCode = BadgeFactory.bytecode + deployArguments.slice(2,);
+    // let salt = abi.encode(["uint256"], [666])
+    let hashedBytecode = ethers.utils.keccak256(rawByteCode)
+    contractName = "VotingPlayground"
+    let PlaygroundFactory = await ethers.getContractFactory("VotingPlayground")
+    let playground: VotingPlayground = await PlaygroundFactory.connect(signer).deploy(
+        registryAddress,
+        flagAndSelectors,
+        votingContracts,
+        minDurations,
+        minQuorums,
+        badgeWeightedVote,
+        hashedBytecode
+    )
+    await playground.deployed()
+    
+    info = Object.assign(info, { "VotingPlayground": 
+      {
+        "address": playground.address,
+        "path": basePath + "examples/playground/Playground.sol",
+        "arguments": [
+          `"${registryAddress}"`,
+          `[${flagAndSelectors.map((v)=>{return `"${v}"`})}]`,
+          `[${votingContracts.map((v)=>{return `"${v}"`})}]`,
+          `[${minDurations}]`,
+          `[${minQuorums}]`,
+          `[${badgeWeightedVote}]`,
+          `"${hashedBytecode}"`
+        ]} 
+    });
+    
+    if (verbosity>0) console.log(`--> Deployed ${contractName}`)
+    if (verbosity>1) console.log(`\t address: ${playground.address}\n\t network: ${network.name}`)
+    if (verbosity>1) console.log(`\t deployNewBadgeSelector: ${playgroundInterface.getSighash("deployNewBadge")}}`)
+    
+    let tx = await playground.connect(signer).deployNewBadge(ethers.constants.HashZero, rawByteCode, signer.address)
+    await tx.wait()
+
+    contractName = "PlaygroundVotingBadge"
+    let badge = await ethers.getContractAt("PlaygroundVotingBadge", await playground.badges(0));
+    
+    info = Object.assign(info, { "PlaygroundVotingBadge": 
+      {
+        "address": badge.address,
+        "path": basePath + "examples/playground/Playground.sol",
+        "arguments": [`"${deployArgumentArray[0]}"`, `"${deployArgumentArray[1]}"`]
+      }}) 
+    
+    if (verbosity>0) console.log(`--> Deployed ${contractName}`)
+    if (verbosity>1) console.log(`\t address: ${badge.address}\n\t network: ${network.name}`)
+
+    return info
+}
+
+
+
+export { 
+  deployPlaygroundAndBadge
+}
---
+++ scripts/deployment/registry.ts
@@ -0,0 +1,35 @@
+import { ethers, network } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { IVOTINGCONTRACT_ID, IERC165_ID } from "../utils/interfaceIds";
+import { ContractDeploymentInfo } from "../interfaces/deployment"
+
+import {  basePath } from "../utils/paths";
+
+
+import {
+  VotingRegistry
+} from "../../typechain"
+
+
+
+async function deployRegistry(signer: SignerWithAddress, verbosity: number): Promise<ContractDeploymentInfo>{
+    let contractName = "VotingRegistry"
+    let RegistryFactory = await ethers.getContractFactory("VotingRegistry")
+    let registry: VotingRegistry = await RegistryFactory.connect(signer).deploy(IVOTINGCONTRACT_ID)
+    await registry.deployed() 
+    let info: ContractDeploymentInfo = {
+        "VotingRegistry": {
+            "address": registry.address,
+            "path": basePath + "registration/registry/VotingRegistry.sol",
+            "arguments": [`"${IVOTINGCONTRACT_ID}"`]} 
+    }
+    
+    if (verbosity>0) console.log(`--> Deployed ${contractName}`)
+    if (verbosity>1) console.log(`\t address: ${registry.address}\n\t network: ${network.name}`)
+    
+    return info
+}
+
+export {
+    deployRegistry
+}
